2d23530f5ca53c2c763841886291a0e7888a41f0
erfan <erfank20041382@gmail.com>
2024-07-28 04:43:37 +0430

    kir

--- backend/api/views.py
+++ backend/api/views.py
@@ -99,7 +99,7 @@ class ReservationView(APIView):
     @permission_classes([permissions.IsAuthenticated])
     def get(self,request:Request):
         now=jdatetime.datetime.today()
-        reservations=Reservation.objects.filter(user=request.user,date__lte=F('shift_meal__date'))
+        reservations=Reservation.objects.filter(user=request.user,date__lte=F('shift_meal__date'),shift_meal__date__gte=now)
         serialized=ReservationSerializer(reservations,many=True,context={"request":request})
         return Response(data=serialized.data,status=status.HTTP_200_OK)
     
